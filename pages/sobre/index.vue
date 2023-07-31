0d375e3825867e3e064d39c9455711f50f5d8c0b
Victor Tavares <victortavaresdev@gmail.com>
2023-07-31 16:44:00 -0300

    updates

--- pages/sobre/index.vue
+++ pages/sobre/index.vue
@@ -2,8 +2,57 @@
 
 <template>
   <section class="w-full min-h-[90vh] bg-slate-100">
-    <div class="px-8 py-12">
+    <div class="px-8 py-12 flex flex-col gap-6 w-full max-w-5xl">
       <TheHeading title="sobre" />
+
+      <div>
+        <h2 class="font-bold text-slate-900 text-xl mb-2">O que é QuizDev?</h2>
+
+        <p class="mb-2 text-slate-900">
+          QuizDev é um emocionante jogo de código aberto com foco em quizzes de programação que visa
+          fornecer uma plataforma divertida e desafiadora para desenvolvedores de todos os níveis de
+          habilidade poderem testar seus conhecimentos técnicos.
+        </p>
+        <p class="text-slate-900">
+          Seja você um iniciante procurando aprimorar suas habilidades técnicas ou um desenvolvedor
+          experiente em busca de um novo desafio, a Quiz Dev é para você.
+        </p>
+      </div>
+
+      <div>
+        <h2 class="font-bold text-slate-900 text-xl mb-2">Quais as regras do jogo?</h2>
+
+        <ul class="text-slate-900">
+          <li>
+            1 - Responda à pergunta selecionando uma das
+            <span class="font-bold">4 opções de resposta</span>.
+          </li>
+          <li>2 - Responda a pergunta em menos de <span class="font-bold">10 segundos</span>.</li>
+          <li>
+            3 - As <span class="font-bold">pontuações</span> são mostradas na parte superior do
+            quiz.
+          </li>
+          <li>4 - Cada pergunta vale <span class="font-bold">1 ponto</span>.</li>
+          <li>5 - Um <span class="font-bold">quiz completo</span> consiste em 5 perguntas.</li>
+        </ul>
+      </div>
+
+      <div>
+        <h2 class="font-bold text-slate-900 text-xl mb-2">Quer contribuir?</h2>
+
+        <p class="text-slate-900">
+          Como um projeto de código aberto, a QuizDev prospera com o apoio de sua comunidade. Se
+          você está entusiasmado em aprimorar o jogo ou adicionar novos recursos, convidamos você a
+          contribuir com o projeto. Confira o repositório do GitHub
+          <a
+            href="https://github.com/victortavaresdev"
+            target="_blank"
+            class="text-blue-600 font-bold"
+            >aqui</a
+          >
+          para começar.
+        </p>
+      </div>
     </div>
   </section>
 </template>
