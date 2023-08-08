d0f9152115a33f53f4d8a434c910a508dee809f3
Victor Tavares <victortavaresdev@gmail.com>
2023-08-07 21:25:44 -0300

    updates

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -9,9 +9,9 @@ export const useApiFetch = <T>(path: string, options?: UseFetchOptions<T>) => {
     headers.Authorization = `Bearer ${accessToken}`
   }
 
-  return useFetch(`http://localhost:8000/api/v1/${path}`, {
+  return useFetch(`https://quiz-dev.up.railway.app/api/v1/${path}`, {
     credentials: 'include',
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://quiz-dev.up.railway.app',
     watch: false,
     ...options,
     headers: {
