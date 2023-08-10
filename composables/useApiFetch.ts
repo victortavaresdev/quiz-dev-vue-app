f0f1e47bbf52178b964cefc3651f090d1cbf5ae8
Victor Tavares <victortavaresdev@gmail.com>
2023-08-10 18:58:24 -0300

    updates

--- composables/useApiFetch.ts
+++ composables/useApiFetch.ts
@@ -3,15 +3,17 @@ import type { UseFetchOptions } from 'nuxt/app'
 export const useApiFetch = <T>(path: string, options?: UseFetchOptions<T>) => {
   let headers: { Authorization?: string } = {}
 
+  const BASE_URL = process.env.BASE_URL || 'http://localhost:8000'
+
   const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
   if (token) {
     const accessToken = JSON.parse(token)?.accessToken
     headers.Authorization = `Bearer ${accessToken}`
   }
 
-  return useFetch(`http://localhost:8000/api/v1/${path}`, {
+  return useFetch(`${BASE_URL}/api/v1/${path}`, {
     credentials: 'include',
-    baseURL: 'http://localhost:8000',
+    baseURL: `${BASE_URL}`,
     watch: false,
     ...options,
     headers: {
