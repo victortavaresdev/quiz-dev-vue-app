55de0ee994bd1fd6e17f1879d539e4372af6a55a
Victor Tavares <victortavaresdev@gmail.com>
2023-08-12 21:23:59 -0300

    updates

--- middleware/reset-password.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  const { token } = useRoute().query
-
-  if (!token && to.path === '/resetar-senha') {
-    return navigateTo('/')
-  }
-})
